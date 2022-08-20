b725f7d7c0bd3b83b04627bed411732131fd2de9
Kevin <jung.kevinj@gmail.com>
2022-08-20 13:35:42 -0700

    Completion of responsive navbar with toggle functionality for bringing out nav from the side. Imported Fonts and images folder for assets.

---
+++ index.js
@@ -0,0 +1,18 @@
+const navToggle = () => {
+  const toggleBtn = document.querySelector(".toggle-btn")
+  const exitBtn = document.querySelector(".close-btn")
+  const navLinksContainer = document.querySelector(".navbar-links-container")
+  
+  const toggler = (element) => {
+    element.addEventListener("click", () => {
+      exitBtn.classList.toggle("show")
+      toggleBtn.classList.toggle("hide")
+      navLinksContainer.classList.toggle("nav-slide")
+    })
+  }
+  
+  toggler(toggleBtn)
+  toggler(exitBtn)
+}
+
+navToggle()
